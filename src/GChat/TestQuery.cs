4e4a12da2346238609e808bdc9dae2a7ff2c5b43
Max Geuer <geuermax@gmail.com>
2021-02-22 21:46:52 +0100

    bootstrap chatapplication

---
+++ src/GChat/TestQuery.cs
@@ -0,0 +1,12 @@
+﻿using HotChocolate.AspNetCore.Authorization;
+using HotChocolate.Types;
+
+namespace GChat
+{
+    [ExtendObjectType(Name = "Query")]
+    [Authorize]
+    public class TestQuery
+    {
+        public string GetTest() => "test";
+    }
+}
