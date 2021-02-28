f7a0d565f3818e3b73136407f7ebf8159ae5a242
Max Geuer <36498600+geuermax@users.noreply.github.com>
2021-02-28 12:26:27 +0100

    Added common classes for further mutations

---
+++ src/GChat/Graphql/Common/Payload.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GChatAPI.Graphql.Common
+{
+    public class Payload
+    {
+        public IReadOnlyList<UserError>? Errors { get; }
+
+
+        public Payload(IReadOnlyList<UserError>? errors = null)
+        {
+            this.Errors = errors;
+        }
+    }
+}
---
+++ src/GChat/Graphql/Common/UserError.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GChatAPI.Graphql.Common
+{
+    public class UserError
+    {
+        public string Message { get; }
+        public string Code { get; }
+
+        public UserError(string message, string code)
+        {
+            this.Message = message;
+            this.Code = code;
+        }
+    }
+}
