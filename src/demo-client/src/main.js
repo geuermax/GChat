4c317ee395968e913e127e9dd4cf03d587a3e2c3
Max Geuer <36498600+geuermax@users.noreply.github.com>
2021-02-28 17:45:17 +0100

    Added appollo and keycloak dependencies with configuration

--- src/demo-client/src/main.js
+++ src/demo-client/src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { createProvider } from './vue-apollo'
+import Keycloak from 'keycloak-js';
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+let initOptions = {
+  url: 'http://localhost:80/auth',
+  realm: 'master',
+  clientId: 'api-dev'
+}
+
+var keycloak = new Keycloak(initOptions);
+keycloak.init()
+  .then(function (authenticated) {
+    if (!authenticated) {
+      keycloak.login();
+    } else {
+      window.keycloak = keycloak;
+      const apolloProvider = createProvider();
+      new Vue({
+        apolloProvider: apolloProvider,
+        render: h => h(App)
+      }).$mount('#app')
+    }
+  });
