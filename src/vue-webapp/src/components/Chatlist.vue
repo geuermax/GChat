dd61df9d2df480629c276b9a8f7e8fd9722a1c05
Max Geuer <36498600+geuermax@users.noreply.github.com>
2021-03-22 20:55:43 +0100

    created basic folder structure

---
+++ src/vue-webapp/src/components/Chatlist.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+    name:'Chatlist'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/vue-webapp/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mx-header">
+      
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+
+</style>
